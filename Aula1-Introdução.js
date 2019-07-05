8c09f3f048df5b7ff16656d8870843948d9b55af
Fernando Mazola <fernando_mazola@hotmail.com>
2019-07-05 02:10:59 -0300

    Aprendendo ECMA Aula 1, principais novidades no ES6

---
+++ Aula1-Introdução.js
@@ -0,0 +1,16 @@
+//ECMA é a linguagem de script padronizada pela ECMAScript International
+
+// - Suporte para constantes
+// - Bloco de escopo
+// - Arrow Function / Funções lambdas - Sintaxe mais curta não tem seu próprio this, arguments, super ou new.target. Estas expressões de funções são melhor aplicadas para funções que não sejam métodos, e elas não podem ser usadas como construtoras (constructors).
+// - Manipulação Estendida de Parâmetros - Multiplus parametros de maneira mais facil
+// - Template Strings
+// - Propriedades aprimoradas do objeto
+// - Atribuição de desestruturação
+// - Módulos - Agora pode ser modularizada, não precisa mais criar script e puxar o script de jquery, angular etc, podemos usa-los como modulo _reset.js e importar no arquivo principal js
+// - Classes
+// - Iteradores 
+// - Funções Geradoras
+// - Coleções (Map, Set)
+// - Novos métodos construídos para várias classes
+// - Promises (Funções assíncronas)
