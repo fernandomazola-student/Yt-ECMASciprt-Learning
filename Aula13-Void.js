08047c9b3f952cb25cccfc8e640f69d2c66fcea0
Fernando Mazola <fernando_mazola@hotmail.com>
2019-07-09 18:54:31 -0300

    Aprendendo sobre void javascript

---
+++ Aula13-Void.js
@@ -0,0 +1,9 @@
+/**
+ Como na maioria das lnguiagens, quando nao queremos retornar, podemos retornar void
+ */
+
+ //Necessariamente, nao precisamos retornar algo, eu mato a função em relação a retorno. Se deixarmos em branco, ela é void tbm
+ //mas temos a opção de usar o retorno, nesse caso não.
+ void function name(params) {
+     
+ }
